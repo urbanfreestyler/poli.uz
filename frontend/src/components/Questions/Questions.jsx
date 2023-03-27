8a9e8b4f48396edc8eb6ba3ae0b82d28e1412fff
urbanfreestyler <rashidov.b19@gmail.com>
2023-03-27 11:56:46 +0500

    add result

--- frontend/src/components/Questions/Questions.jsx
+++ frontend/src/components/Questions/Questions.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { getQuestions, getAnswers } from "../queries";
+import { Link } from "react-router-dom";
 
-function Question() {
+import Navbar from "../Navbar/Navbar";
+import { getQuestions, getAnswers } from "../queries";
+import "./Questions.css";
+
+const Question = () => {
   const { quiz_id } = useParams();
 
   const [questions, setQuestions] = useState(null);
@@ -14,14 +18,16 @@ function Question() {
   const [incorrectAnswers, setIncorrectAnswers] = useState({});
 
   const handleAnswer = (answer) => {
-    if (answer.question in correctAnswers) {
-      delete correctAnswers[answer.question];
-    } else if (answer.question in incorrectAnswers) {
+    if (answer.is_correct) {
+      answer.id === correctAnswers[answer.question]
+        ? delete correctAnswers[answer.question]
+        : (correctAnswers[answer.question] = answer.id);
       delete incorrectAnswers[answer.question];
     } else {
-      answer.is_correct === true
-        ? (correctAnswers[answer.question] = true)
-        : (incorrectAnswers[answer.question] = true);
+      answer.id === incorrectAnswers[answer.question]
+        ? delete incorrectAnswers[answer.question]
+        : (incorrectAnswers[answer.question] = answer.id);
+      delete correctAnswers[answer.question];
     }
 
     console.log(correctAnswers);
@@ -29,10 +35,9 @@ function Question() {
   };
 
   const showResults = () => {
-    const result = Math.floor(
+    const result =
       1.5 * Object.keys(correctAnswers).length -
-        0.5 * Object.keys(incorrectAnswers).length
-    );
+      0.5 * Object.keys(incorrectAnswers).length;
     console.log(result);
   };
 
@@ -42,84 +47,150 @@ function Question() {
       setQuestions(questions);
     };
 
+    const activeQuestionDetector = () => {
+      if (questions != null) {
+        questions.map((index) => {
+          if (activeQuestion === index) {
+            document.body.classList.add("selected");
+            console.log(index);
+          }
+        });
+      }
+    };
+
     const answersList = async (pk) => {
       const answers = await getAnswers(pk);
       setAnswers(answers);
     };
-
     questionList(quiz_id);
     answersList(quiz_id);
-  }, [quiz_id]);
+    activeQuestionDetector();
+  }, [quiz_id, activeQuestion, questions]);
 
   const changeQuestion = (index) => {
     setActiveQuestion(index);
   };
 
+  const resultData = {
+    questions: questions,
+    correctAnswers: correctAnswers,
+    incorrectAnswers: incorrectAnswers,
+  };
+
   return (
-    <div>
-      <div className="questions_list">
-        {questions &&
-          questions.map((question, index) => {
-            return (
-              <button
-                key={question.id}
-                disabled={activeQuestion === index ? true : false}
-                onClick={() => {
-                  changeQuestion(index);
-                }}
-              >
-                {index + 1}
-              </button>
-            );
-          })}
+    <>
+      <Navbar />
+
+      <div className="questions__wrapper">
+        <div className="questions__list">
+          <p>Questions</p>
+          <div className="questions__btns">
+            {questions &&
+              questions.map((question, index) => {
+                return (
+                  <button
+                    key={index + 1}
+                    className=""
+                    onClick={() => {
+                      changeQuestion(index);
+                    }}
+                  >
+                    {index + 1}
+                  </button>
+                );
+              })}
+          </div>
+        </div>
+        <div className="questions__answer_wrapper">
+          <div className="container">
+            <div className="questions__content">
+              {questions &&
+                questions.map((question, index) => {
+                  return activeQuestion === index ? (
+                    <div>
+                      Question {index + 1}
+                      <div className="questions__text" key={question.id}>
+                        {question.text}
+                      </div>
+                    </div>
+                  ) : null;
+                })}
+              <div>
+                <ul className="list-group">
+                  {answers &&
+                    answers.map((answer, index) => {
+                      return activeQuestion === answer.question - 1 ? (
+                        <div
+                          className="list-group-item answer"
+                          key={answer.id}
+                          onClick={() => {
+                            handleAnswer(answer);
+                          }}
+                        >
+                          {answer.text}
+                        </div>
+                      ) : null;
+                    })}
+                </ul>
+              </div>
+              {questions &&
+                questions.map((question, index) => {
+                  return activeQuestion === index ? (
+                    activeQuestion === questions.length - 1 ? (
+                      <div>
+                        <button
+                          className="button button-large button-circle button-3d button-dirtygreen"
+                          onClick={() => {
+                            setActiveQuestion(activeQuestion - 1);
+                          }}
+                        >
+                          Prev.
+                        </button>
+                        <Link
+                          className="button button-large button-circle button-3d button-dirtygreen"
+                          to="/result"
+                          state={resultData}
+                        >
+                          Finish
+                        </Link>
+                      </div>
+                    ) : activeQuestion === index && index === 0 ? (
+                      <button
+                        className="button button-large button-circle button-3d button-dirtygreen"
+                        onClick={() => {
+                          setActiveQuestion(activeQuestion + 1);
+                        }}
+                      >
+                        Next
+                      </button>
+                    ) : (
+                      <div>
+                        <button
+                          className="button button-large button-circle button-3d button-dirtygreen"
+                          onClick={() => {
+                            setActiveQuestion(activeQuestion - 1);
+                          }}
+                        >
+                          Prev.
+                        </button>
+                        <button
+                          className="button button-large button-circle button-3d button-dirtygreen"
+                          onClick={() => {
+                            setActiveQuestion(activeQuestion + 1);
+                          }}
+                        >
+                          Next
+                        </button>
+                      </div>
+                    )
+                  ) : null;
+                })}
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="questions_list">
-        {questions &&
-          questions.map((question, index) => {
-            return activeQuestion === index ? (
-              <div key={question.id}>{question.text}</div>
-            ) : null;
-          })}
-      </div>
-      <ul>
-        {answers &&
-          answers.map((answer, index) => {
-            return activeQuestion === answer.question - 1 ? (
-              <li
-                style={{ padding: "15px" }}
-                key={answer.id}
-                onClick={() => {
-                  handleAnswer(answer);
-                }}
-              >
-                answer: {answer.text}
-              </li>
-            ) : null;
-          })}
-      </ul>
-      {questions &&
-        questions.map((question, index) => {
-          return activeQuestion === index &&
-            activeQuestion < questions.length - 1 ? (
-            <button
-              onClick={() => {
-                setActiveQuestion(activeQuestion + 1);
-              }}
-            >
-              Next
-            </button>
-          ) : activeQuestion === index ? (
-            <button
-              onClick={() => {
-                showResults();
-              }}
-            >
-              Finish
-            </button>
-          ) : null;
-        })}
-    </div>
+    </>
   );
-}
+};
 
 export default Question;
--- frontend/src/components/Result/Result.jsx
+++ frontend/src/components/Result/Result.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import "./Result.css";
+const Result = (props) => {
+  const location = useLocation();
+  const resultData = location.state;
+  const questions = resultData.questions;
+  const correctAnswers = resultData.correctAnswers;
+  const incorrectAnswers = resultData.incorrectAnswers;
 
-function Result() {
-  return <div>Result</div>;
-}
+  const Results = () => {
+    const correctPoints = Object.keys(correctAnswers).length;
+    const incorrrectPoints = Object.keys(incorrectAnswers).length;
+
+    const result = 1.5 * correctPoints - 0.5 * incorrrectPoints;
+
+    console.log(correctPoints);
+    console.log(incorrrectPoints);
+    console.log(result);
+
+    return (
+      <div>
+        <div>Correct answers: {correctPoints}</div>
+        <div>Incorrect answers: {incorrrectPoints}</div>
+        <div>Result: {result}</div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="result__container">
+        <div>YourResult</div>
+        <Results />
+      </div>
+    </>
+  );
+};
 
 export default Result;
